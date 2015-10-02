fc7a558ce7c8e37cbeb7783e41520de9727a1a7d
Victor Wilson Pinto Bastos <vbastos@sorocaba.sp.gov.br>
2015-10-02 19:05:51 -0300

    check if select is a block helper or not

--- hbs-fields/select.js
+++ hbs-fields/select.js
@@ -4,7 +4,11 @@ var Factory = require('./factory');
 Handlebars.registerHelper('select', function(attrs) {
 	attrs.hash.type = attrs.hash.type || 'text';
 
-	var element = $('<select/>').html(attrs.fn(this));
+	var element = $('<select/>');
+
+	if(attrs.fn) {
+		element = element.html(attrs.fn(this));
+	}
 
 	return new Handlebars.SafeString(Factory(element, attrs));
 });
